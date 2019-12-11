f5f6ced7566b87e84e2689df44109e8a14c5db87
Javier Lindo <javier.lindo@sngular.com>
2019-12-11 10:22:53 +0100

    Changing Pom.xml

--- src/main/java/com/viriato/courses/CoursesApplication.java
+++ src/main/java/com/viriato/courses/CoursesApplication.java
@@ -3,14 +3,21 @@ package com.viriato.courses;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CoursesApplication {
+public class CoursesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursesApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoursesApplication.class);
+    }
 
 	@Bean
 	public ModelMapper modelMapper() {
